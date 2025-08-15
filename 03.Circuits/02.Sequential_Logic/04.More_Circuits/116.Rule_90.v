e4d6a9750403b071962e1a18b149ad2a82f2a12b
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-08-15 11:23:45 +0530

    Create 116.Rule_90.v

---
+++ 03.Circuits/02.Sequential_Logic/04.More_Circuits/116.Rule_90.v
@@ -0,0 +1,11 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q
+); 
+always @(posedge clk)
+    if (load) q <= data;
+    else q <= (q >> 1) ^ (q << 1);
+  
+endmodule
