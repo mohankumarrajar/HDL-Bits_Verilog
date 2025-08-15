f193f28a7e6acccafb679cfc745daab6f11f74d5
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-08-15 10:57:17 +0530

    Create 97.Even_Capture_Register.v

---
+++ 03.Circuits/02.Sequential_Logic/01.Latches_And_Flip-Flops/97.Even_Capture_Register.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input reset,
+    input [31:0] in,
+    output [31:0] out
+);
+    integer i;
+    reg [31:0] last;
+  
+    always @(posedge clk) begin
+        last <= in;
+      if (reset) 
+        out <= 0;
+      else for (i = 0; i < 32; i = i + 1)
+        out[i] <= last[i] & ~in[i] ? 1 : out[i];
+end
+  
+endmodule
