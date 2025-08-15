5880faad123c02a954fd7d656be418ec5d9e76c2
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-08-15 10:44:10 +0530

    Create 87.D_Latch.v

---
+++ 03.Circuits/02.Sequential_Logic/01.Latches_And_Flip-Flops/87.D_Latch.v
@@ -0,0 +1,14 @@
+module top_module (
+    input d, 
+    input ena,
+    output q
+);
+  
+    always@(ena)begin
+        if(ena)
+            q<=d;
+        else
+            q<=q;
+    end
+  
+endmodule
