e55552393d91155c4d3c67e89232ad3c06dfc7fe
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-08-16 10:19:54 +0530

    Create 141.Serial_Two's_Complementer(Mealy_FSM).v

---
+++ 03.Circuits/02.Sequential_Logic/05.Finite_State_Machines/141.Serial_Two's_Complementer(Mealy_FSM).v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input areset,
+    input x,
+    output z
+); 
+reg cs, ns;
+always @(posedge clk or posedge areset)
+    if (areset) cs <= 1'b0;
+    else cs <= ns;
+always @(*)
+    if (cs) ns = 1'b1;
+    else ns = x;
+assign z = x ^ cs;
+
+endmodule
